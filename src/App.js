f870c2a698f06898e5b9205c88f0ad4a85456cf0
sal.marcelonardi <sal.marcelonardi@gmail.com>
2021-08-17 18:14:37 -0300

    adding files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Home from './Pages/Home';
+import Routes from './routes';
+ 
+function App() {
+  return (
+    <Routes />
+  )
+}
+ export default App;
